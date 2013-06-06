c32d2d83bc107b6a2228fe2c68517bfd70374b9d
xdanielDZD <xdanielDZD@googlemail.com>
2013-06-06 17:51:01 +0000

    Merging local changed not yet committed (new ProjectHandler classes, VERY preliminary; some changes to model loading), also making memo to myself to always "commit all the things!" :P

--- Labyrinth/ModelImport/I3DModel.cs
+++ Labyrinth/ModelImport/I3DModel.cs
@@ -7,6 +7,7 @@ namespace Labyrinth.ModelImport
 {
     interface I3DModel
     {
+        void Open(string fn);
         Common.Model Model { get; set; } //end method
     }//end interface
 }//end namespace
---
+++ Labyrinth/ProjectHandler/ProjectHelpers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Labyrinth.ProjectHandler
+{
+    class ProjectHelpers
+    {
+        public static ModelImport.I3DModel OpenModelByExtension(string fn)
+        {
+            ModelImport.I3DModel model = null;
+            string ext = System.IO.Path.GetExtension(fn);
+
+            switch (ext)
+            {
+                case ".obj":
+                    model = new ModelImport.WavefrontObj(fn);
+                    break;
+
+                default:
+                    throw new Exception("Unsupported model format");
+            }
+
+            return model;
+        }
+    }
+}
